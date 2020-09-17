c52de5b6da487d5fa92aa3d618076c6013ff14f2
Li Bin <lbbc1117@163.com>
2020-09-17 10:58:12 +0800

    Add files via upload
    init commit

---
+++ marriage.py
@@ -0,0 +1,53 @@
+from random import uniform, triangular
+from statistics import mean
+
+
+def float_range(start, stop, steps):
+    return [start + float(i) * (stop - start) / (float(steps) - 1) for i in range(steps)]
+
+
+VALUES = float_range(0, 1, 11)
+GAME_TOTAL = 1000
+
+
+def get_usefulness(v1, v2):
+    # -(v1+v2)  惩罚拜金价值观
+    # v1*v2     惩罚婚姻双方的差异
+    # 最后用系数配平
+    return 2.0 - 2.0 * (v1 + v2) + 3.0 * v1 * v2
+
+
+def print_result(result, distribution, args):
+    print("*" * 50)
+    print("Result with {0}{1}".format(distribution, args))
+    print("*" * 50)
+    for v1 in VALUES:
+        _key = str(v1)
+        print("value: {0} -- avg: {1} | max: {2}".format(
+            v1, format(result[_key]["avg"], ".2f"), format(result[_key]["max"], ".2f")))
+    print("")
+
+
+def simulate(distribution, args):
+    result = {}
+    for v1 in VALUES:
+        _key = str(v1)
+        result[_key] = {}
+        result[_key]["max"] = -99
+        result[_key]["avg"] = []
+        for _ in range(GAME_TOTAL):
+            v2 = distribution(*args)
+            usefulness = get_usefulness(v1, v2)
+            result[_key]["max"] = result[_key]["max"] if usefulness < result[_key]["max"] else usefulness
+            result[_key]["avg"].append(usefulness)
+        result[_key]["avg"] = mean(result[_key]["avg"])
+    print_result(result, distribution.__name__, args)
+
+
+if __name__ == "__main__":
+    # 均匀分布
+    simulate(uniform, (0, 1))
+    # 三角分布
+    modes = float_range(0, 1, 5)
+    for mode in modes:
+        simulate(triangular, (0.0, 1.0, mode))
